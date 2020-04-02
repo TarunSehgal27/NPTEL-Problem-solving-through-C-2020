96ce67f3b66fbb48f95547dacdc644c3979c05e0
Tarun Sehgal <tarunsehgal27@gmail.com>
2020-04-02 22:23:53 +0530

    bugs fixed

--- WEEK 09/que2.c
+++ WEEK 09/que2.c
@@ -1,36 +1,29 @@
 #include <stdio.h>
- 
-long linear_search(long [], long, long);
- 
+int linear_search(int[], int, int);
 int main()
 {
-   long array[100], search, c, n, position;
- 
-   printf("Input number of elements in array\n");
-   scanf("%ld", &n);
- 
-   printf("Input %d numbers\n", n);
- 
-   for (c = 0; c < n; c++)
-      scanf("%ld", &array[c]);
- 
-   printf("Input a number to search\n");
-   scanf("%ld", &search);
- 
-   position = linear_search(array, n, search);
- 
-   if (position == -1)
-      printf("%d isn't present in the array.\n", search);
+   int array[100], search, c, n, position;
+   /* search - element to search, c - counter, n - number of elements in array,
+   position - The position in which the element is first found in the list. */
+
+    scanf("%d", &n); // Number of elements in the array is read from the test case data
+
+    for (c = 0; c < n; c++)
+    scanf("%d", &array[c]); //Elements of array is read from the test data
+
+    scanf("%d", &search); 
+ position = linear_search(array, n, search);
+if (position == -1)
+     printf("%d is not present in the array.\n", search);
    else
-      printf("%d is present at location %d.\n", search, position+1);
- 
-   return 0;
+     printf("%d is present at location %d.\n", search, position+1);
+return 0;
 }
  
-long linear_search(long a[], long n, long find) {
-   long c;
- 
-   for (c = 0 ;c < n ; c++ ) {
+int linear_search(int a[], int n, int find) {
+   int c;
+   for (c = 0 ;c < n ; c++ ) 
+   {
       if (a[c] == find)
          return c;
    }
